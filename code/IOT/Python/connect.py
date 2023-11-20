eb5a5e6dcf5fa9acb52fd95bf1f1c70fcd8456ef
AhmadK <126020725+Marshall331@users.noreply.github.com>
2023-11-20 10:48:51 +0100

    écriture des alertes en début du fichier

--- code/IOT/Python/connect.py
+++ code/IOT/Python/connect.py
@@ -4,7 +4,6 @@ import configparser
 import os
 import time
 from datetime import datetime
-import signal
 
 # Chemin du fichier de configuration
 config_file_path = r'code\IOT\Python\config.ini'
@@ -28,8 +27,8 @@ print("Sections trouvées :", config.sections())
 broker = config.get('MQTT', 'broker')
 port = config.getint('MQTT', 'port')
 topic = config.get('MQTT', 'topic')
-choixDonnees = config.get('MQTT', 'choixDonnees').split(',')  # Récupère les valeurs à afficher
-print("Vous avez choisi d'afficher les données suivante : ", choixDonnees)
+choix_donnees = config.get('MQTT', 'choix_donnees').split(',')  # Récupère les valeurs à afficher
+print("Vous avez choisi d'afficher les données suivante : ", choix_donnees)
 
 # Dictionnaire pour stocker les valeurs
 values_by_room = {}
@@ -41,9 +40,6 @@ file_name = os.path.join(config_dir, "donnee.txt")
 # Supprimer un fichier existant
 if os.path.exists(file_name):
     os.remove(file_name)
-
-
-pending_data = {}
 
 # Fonction pour calculer la moyenne des 10 dernières valeurs
 def calculer_moyenne(historique):
@@ -57,17 +53,21 @@ def ecrire(room, data):
                 file.write(f"\nSalle : {room}\n")
                 values_by_room[room] = []  # Initialise la liste pour la nouvelle salle
             file.write(data + "\n")
-        #time.sleep(frequence_affichage)  # Attendre avant d'afficher la prochaine salle
+        time.sleep(frequence_affichage)  # Attendre avant d'afficher la prochaine salle
     except Exception as e:
         print(f"Erreur lors de l'écriture dans le fichier : {e}")
 
-def handler(signum, frame):
-    for room, data in pending_data.items():
-        ecrire(room, data)  
-    signal.alarm(frequence_affichage)
-signal.signal(signal.SIGALRM, handler)
-
-
+def ecrire_alerte(alerte):
+    try:
+        alert_file = os.path.join(config_dir, "Alerte.txt")
+        with open(alert_file, "r", encoding="utf-8") as file:
+            content = file.read()  # Lire le contenu actuel du fichier
+        with open(alert_file, "w", encoding="utf-8") as file:
+            file.write(alerte + "\n")  # Ajouter la nouvelle alerte au début
+            file.write(content)  # Réécrire le contenu actuel après la nouvelle alerte
+    except Exception as e:
+        print(f"Erreur lors de l'écriture dans le fichier d'alerte : {e}")
+        
 # Fonction appelée lors de la connexion au broker
 def on_connect(client, userdata, flags, rc):
     print("Connecté avec le code résultat " + str(rc))
@@ -88,10 +88,11 @@ def on_message(client, userdata, msg):
         if room not in historique_par_salle:
             historique_par_salle[room] = {key: [] for key in sensor_data.keys()}
 
-        # Mise à jour des historiques et calcul des moyennes
+        alerte_texte = f"Alertes pour {room}:\n"
         texte = f"Valeurs pour {room}:\n"
+
         for key, value in sensor_data.items():
-            if key.lower() in choixDonnees:  # Vérifie si la clé est dans les valeurs à afficher
+            if key.lower() in choix_donnees:  # Vérifie si la clé est dans les valeurs à afficher
                 historique_par_salle[room][key].append(value)
                 # S'assurer que la liste ne contient que les 10 dernières valeurs
                 if len(historique_par_salle[room][key]) > 10:
@@ -99,11 +100,17 @@ def on_message(client, userdata, msg):
                 moyenne = calculer_moyenne(historique_par_salle[room][key])
                 texte += f"{key}: {value}, Moyenne (10 dernières): {moyenne}\n"
 
+                # Vérification des seuils pour déclencher une alerte
+                seuil_key = f"seuil_{key.lower()}"
+                if config.has_option('MQTT', seuil_key):
+                    seuil_max = config.getint('MQTT', seuil_key)
+                    if value > seuil_max:
+                        alerte_texte += f"{key} a dépassé le seuil maximum de {seuil_max}. Valeur actuelle : {value}\n"
+
+        if len(alerte_texte) > len(f"Alertes pour {room}:\n"):
+            ecrire_alerte(alerte_texte)  # S'il y a des alertes, les écrire dans le fichier
+
         print(texte)
-
-        pending_data[room] = texte
-        signal.alarm(0)
-
         ecrire(room, texte)
 
     except Exception as e:
