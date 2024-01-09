51f0ba8cba7b67e51ffbb5dd99d73f240fdce35b
AdrielMarchant <Adriel.tahiti@gmail.com>
2024-01-09 13:34:14 +0100

    bdd

---
+++ code/Web/HTML-PHP/CypressCucumber/cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  video: false,
+  defaultCommandTimeout: 5000,
+  pageLoadTimeout: 10000,
+  ignoreTestFiles: ["*.js","*.md"],
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+  
+});
---
+++ code/Web/HTML-PHP/CypressCucumber/cypress/intergration/cucumber-tests/login/loginSteps.js
@@ -0,0 +1,20 @@
+import { Given, When, Then} from 'cypress-cucumber-preprocessor/steps'
+
+Given('I open login page',()=> {
+    cy.visit('http://193.54.227.208/~saephp12/FormConnexion.php')
+})
+
+When('I submit login',() =>{
+    //fill username
+    cy.get('#username').type('username')
+    //fill password
+    cy.get('#password').type('password')
+    //submit form
+    cy.get('input[name="connexion"]').click()
+})
+
+//devrait etre dans homepageSteps.js
+Then('I should see homepage',() =>{
+    //get some element after login
+    cy.getByText('Déconnexion').parent().should('be.visible')
+})
