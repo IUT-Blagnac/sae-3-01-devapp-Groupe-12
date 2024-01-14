54f98203e6328965392de4994a163d326bd26534
AdrielMarchant <adriel.tahiti@gmail.com>
2024-01-14 17:34:29 +0100

    essai cucumber + readme

--- code/Web/HTML-PHP/CypressCucumber/cypress/e2e/login/loginSteps.js
+++ code/Web/HTML-PHP/CypressCucumber/cypress/e2e/login/loginSteps.js
@@ -1,19 +1,18 @@
-import { Given, When, Then} from 'cypress-cucumber-preprocessor/steps'
+import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
 
-Given('I open login page',()=> {
-    cy.visit('http://193.54.227.208/~saephp12/FormConnexion.php')
-})
+Given('I open login page', () => {
+  cy.visit('http://193.54.227.208/~saephp12/FormConnexion.php');
+});
 
-When('I submit login',() =>{
-    //fill username
-    cy.get('#username').type('username')
-    //fill password
-    cy.get('#password').type('password')
-    //submit form
-    cy.get('input[name="connexion"]').click()
-})
+When('I submit login', () => {
+  // fill username
+  cy.get('#username').type('username');
+  // fill password
+  cy.get('#password').type('password');
+  // submit form
+  cy.get('input[name="connexion"]').click();
+});
 
-//devrait etre dans homepageSteps.js
-Then('I should see homepage',() =>{
-    cy.get('.menu-item').should('be.visible')
-})
+Then('I should see homepage', () => {
+  cy.get('.menu-item').should('be.visible');
+});
